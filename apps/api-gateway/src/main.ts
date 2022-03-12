68e9176f417f79719d7225e2f422660556b77bc7
Phil Star <vipfilstar@gmail.com>
2022-03-13 00:59:44 +0300

    Configure logger

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,11 +6,30 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { utilities, WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+const { combine, ms } = format;
+const { Console } = transports;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new Console({
+          format: combine(
+            ms(),
+            utilities.format.nestLike(environment().api.name, {
+              prettyPrint: true,
+            })
+          ),
+        }),
+      ],
+    }),
+  });
   const configService = app.get(ConfigService);
   const globalPrefix = configService.get<string>('api.prefix');
   app.setGlobalPrefix(globalPrefix);
