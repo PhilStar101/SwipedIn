8707c2b08b5bccdb439cd53c4c192dc781755924
Phil Star <vipfilstar@gmail.com>
2022-03-15 16:40:33 +0300

    Add shared library

---
+++ libs/shared/src/lib/config.module.ts
@@ -0,0 +1,20 @@
+import {
+  ConfigFactory,
+  ConfigModule as ConfigModuleNest,
+  ConfigObject,
+} from '@nestjs/config';
+
+import { environmentSchema } from './environment.schema';
+
+export const ConfigModule = (config: ConfigFactory<ConfigObject>) =>
+  ConfigModuleNest.forRoot({
+    isGlobal: true,
+    expandVariables: true,
+    cache: true,
+    load: [config],
+    validationSchema: environmentSchema,
+    validationOptions: {
+      allowUnknown: true,
+      abortEarly: false,
+    },
+  });
