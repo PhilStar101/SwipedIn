475e07e488876a2f8771bf352c7232813ce9ea90
Phil Star <vipfilstar@gmail.com>
2022-05-17 19:00:28 +0300

    Updated to use new database library

--- apps/profile/src/app.module.ts
+++ apps/profile/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@swiped-in/backend/config';
-import { MongooseModule } from '@swiped-in/shared';
+import { DatabaseModule } from '@swiped-in/backend/database';
 
 import { config } from './config';
 import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [ConfigModule(config), MongooseModule, ProfilesModule],
+  imports: [ConfigModule(config), DatabaseModule, ProfilesModule],
 })
 export class AppModule {}
