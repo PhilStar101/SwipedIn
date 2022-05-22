c9bac97a67a767b4156c9951225e4779bf76c8ea
Phil Star <vipfilstar@gmail.com>
2022-05-22 15:23:48 +0300

    Added TDD for match service

---
+++ apps/api-gateway/src/match/__tests__/match.e2e.spec.ts
@@ -0,0 +1,230 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { DatabaseModule, DatabaseService } from '@swiped-in/backend/database';
+import { CreateMatchDto, Role } from '@swiped-in/shared';
+import { Connection } from 'mongoose';
+import * as request from 'supertest';
+
+import { getEmployee, getHirer } from '../../__mocks__/profiles.mock';
+import { AppModule } from '../../app.module';
+
+describe('Match', () => {
+  let dbConnection: Connection;
+  let httpServer: unknown;
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.setGlobalPrefix('/api');
+    await app.init();
+
+    dbConnection = moduleRef.get<DatabaseService>(DatabaseService).connection;
+    httpServer = app.getHttpServer();
+  });
+
+  afterAll(async () => {
+    await dbConnection.dropDatabase();
+    await app.close();
+  });
+
+  const createMatchDto = (hirerId, employeeId): CreateMatchDto => ({
+    hirer: hirerId,
+    employee: employeeId,
+  });
+
+  const insertMockProfiles = async () => {
+    const inserted: { hirers: string[]; employees: string[] } = {
+      hirers: [],
+      employees: [],
+    };
+
+    const insertedProfiles = await dbConnection
+      .collection('profiles')
+      .insertMany([
+        getHirer(),
+        getHirer(),
+        getHirer(),
+        getEmployee(),
+        getEmployee(),
+        getEmployee(),
+      ]);
+
+    inserted.hirers.push(insertedProfiles.insertedIds[0].toString());
+    inserted.hirers.push(insertedProfiles.insertedIds[1].toString());
+    inserted.hirers.push(insertedProfiles.insertedIds[2].toString());
+    inserted.employees.push(insertedProfiles.insertedIds[3].toString());
+    inserted.employees.push(insertedProfiles.insertedIds[4].toString());
+    inserted.employees.push(insertedProfiles.insertedIds[5].toString());
+    return inserted;
+  };
+
+  beforeEach(async () => {
+    await dbConnection.dropDatabase();
+  });
+
+  describe('createMatch', () => {
+    describe('createHirerMatch', () => {
+      it('should create match for the Hirer', async () => {
+        const inserted = await insertMockProfiles();
+        const createMatchDto: CreateMatchDto = {
+          hirer: inserted.hirers[0],
+          employee: inserted.employees[0],
+        };
+
+        const response = await request(httpServer)
+          .post('/api/match/hirer')
+          .send(createMatchDto);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject({
+          fromRole: Role.Hirer,
+        });
+
+        const hirer = await dbConnection
+          .collection('matches')
+          .findOne({ fromRole: Role.Hirer });
+        expect(hirer).toBeTruthy();
+      });
+    });
+
+    describe('createEmployeeMatch', () => {
+      it('should create match for the Employee', async () => {
+        const inserted = await insertMockProfiles();
+
+        const createMatchDto: CreateMatchDto = {
+          hirer: inserted.hirers[0],
+          employee: inserted.employees[1],
+        };
+
+        const response = await request(httpServer)
+          .post('/api/match/employee')
+          .send(createMatchDto);
+
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject({
+          fromRole: Role.Employee,
+        });
+
+        const hirer = await dbConnection
+          .collection('matches')
+          .findOne({ fromRole: Role.Employee });
+        expect(hirer).toBeTruthy();
+      });
+    });
+  });
+
+  describe('getMatch', () => {
+    const insertMatchFactory = async () => {
+      const insertedIds = await insertMockProfiles();
+      return async (role: Role, hirer: number, employee: number) => {
+        await dbConnection.collection('matches').insertOne({
+          fromRole: role,
+          data: createMatchDto(
+            insertedIds.hirers[hirer],
+            insertedIds.employees[employee],
+          ),
+        });
+        return {
+          hirerId: insertedIds.hirers[hirer],
+          employeeId: insertedIds.employees[employee],
+        };
+      };
+    };
+
+    describe('getHirerMatch', () => {
+      it('should get 1 match(es) when 1 employe(es) accepts 1 hirer(s) and 1 hirer accepts 1 employee(s)', async () => {
+        const insertMatch = await insertMatchFactory();
+        await insertMatch(Role.Employee, 0, 0);
+        const { hirerId } = await insertMatch(Role.Hirer, 0, 0);
+
+        const response = await request(httpServer).get(
+          `/api/match/hirer/${hirerId}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1);
+      });
+
+      it('should get 1 match(es) when 2 employe(es) accepts 1 hirer(s) and 1 hirer accepts 1 employee(s)', async () => {
+        const insertMatch = await insertMatchFactory();
+        await insertMatch(Role.Employee, 0, 0);
+        await insertMatch(Role.Employee, 0, 1);
+        const { hirerId } = await insertMatch(Role.Hirer, 0, 0);
+
+        const response = await request(httpServer).get(
+          `/api/match/hirer/${hirerId}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1);
+      });
+
+      it('should get 1 match(es) when 3 employe(es) accepts 1 hirer(s) and 1 hirer accepts 1 employee(s)', async () => {
+        const insertMatch = await insertMatchFactory();
+        await insertMatch(Role.Employee, 0, 0);
+        await insertMatch(Role.Employee, 0, 1);
+        await insertMatch(Role.Employee, 0, 2);
+        const { hirerId } = await insertMatch(Role.Hirer, 0, 0);
+
+        const response = await request(httpServer).get(
+          `/api/match/hirer/${hirerId}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1);
+      });
+
+      it('should get 2 match(es) when 2 employe(es) accepts 1 hirer(s) and 1 hirer accepts 2 employee(s)', async () => {
+        const insertMatch = await insertMatchFactory();
+        await insertMatch(Role.Employee, 0, 0);
+        await insertMatch(Role.Employee, 0, 1);
+        const { hirerId } = await insertMatch(Role.Hirer, 0, 0);
+        await insertMatch(Role.Hirer, 0, 1);
+
+        const response = await request(httpServer).get(
+          `/api/match/hirer/${hirerId}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(2);
+      });
+
+      it('should get 2 match(es) when 3 employe(es) accepts 1 hirer(s) and 1 hirer accepts 2 employee(s)', async () => {
+        const insertMatch = await insertMatchFactory();
+        await insertMatch(Role.Employee, 0, 0);
+        await insertMatch(Role.Employee, 0, 1);
+        await insertMatch(Role.Employee, 0, 2);
+        const { hirerId } = await insertMatch(Role.Hirer, 0, 0);
+        await insertMatch(Role.Hirer, 0, 1);
+
+        const response = await request(httpServer).get(
+          `/api/match/hirer/${hirerId}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(2);
+      });
+
+      it('should get 3 match(es) when 3 employe(es) accepts 1 hirer(s) and 1 hirer accepts 3 employee(s)', async () => {
+        const insertMatch = await insertMatchFactory();
+        await insertMatch(Role.Employee, 0, 0);
+        await insertMatch(Role.Employee, 0, 1);
+        await insertMatch(Role.Employee, 0, 2);
+        const { hirerId } = await insertMatch(Role.Hirer, 0, 0);
+        await insertMatch(Role.Hirer, 0, 1);
+        await insertMatch(Role.Hirer, 0, 2);
+
+        const response = await request(httpServer).get(
+          `/api/match/hirer/${hirerId}`,
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(3);
+      });
+    });
+  });
+});
