abd68a501efb5be2bebbce6cb4fa2252175bb926
Phil Star <vipfilstar@gmail.com>
2022-05-22 15:23:12 +0300

    Added get functions to mocks

---
+++ apps/api-gateway/src/__mocks__/profiles.mock.ts
@@ -0,0 +1,40 @@
+import { Role } from '@swiped-in/shared';
+
+export const Hirer = {
+  type: Role.Hirer.toString(),
+  name: 'Microsoft',
+  email: 'email_hirer@email.com',
+  webSite: 'webSite.com',
+  stack: ['Node.js', 'JS', 'TS', 'React', 'HTML', 'CSS'],
+  salary: 100000,
+  socials: {
+    instagram: '',
+    twitter: '',
+    gitHub: '',
+    linkedIn: '',
+  },
+};
+
+export const Employee = {
+  type: Role.Employee.toString(),
+  name: {
+    firstName: 'FirstName',
+    lastName: 'LastName',
+  },
+  age: 21,
+  email: 'email_employee@email.com',
+  webSite: 'webSite.com',
+  experience: {
+    time: 2,
+    companies: ['Apple', 'Google'],
+    stack: ['Node.js', 'JS', 'TS', 'React', 'HTML', 'CSS'],
+  },
+  socials: {
+    instagram: '',
+    twitter: '',
+    gitHub: '',
+    linkedIn: '',
+  },
+};
+export const getHirer = () => ({ ...Hirer });
+export const getEmployee = () => ({ ...Employee });
