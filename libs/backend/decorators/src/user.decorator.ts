03e0c5af0be885575fc0a87efc5fa3f63ea3a8a5
Phil Star <vipfilstar@gmail.com>
2022-05-29 21:48:19 +0300

    Added user decorator

--- libs/backend/decorators/src/user.decorator.ts
+++ libs/backend/decorators/src/user.decorator.ts
@@ -4,6 +4,9 @@ export const ReqUser = createParamDecorator(
   (data: string, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     if (data) {
+      if (data === 'id') {
+        return request.user?.sub;
+      }
       return request.user[data];
     }
     return request.user;
